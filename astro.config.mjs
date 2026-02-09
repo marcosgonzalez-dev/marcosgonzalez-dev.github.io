354d3fa1000e20296e5144c56e322d7699f8e18e
marcosgonzalez-dev <marcosgonzalezchavez09@gmail.com>
2026-02-09 14:59:35 -0600

    Arreglo de archivo de configuracion y estilos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://marcosgonzalez-dev.github.io',
 
-  // Agrega esto para cambiar el nombre de la carpeta de estilos
   build: {
     assets: 'assets'
   },
 
   server: {
     port: 4321
-  }
+  },
+
+  vite: {
+    plugins: [tailwind()],
+  },
 });
