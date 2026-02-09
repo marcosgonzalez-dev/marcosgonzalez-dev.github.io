b2e027d4d4df3ab31aa32c160b0150ce89eb15b5
marcosgonzalez-dev <marcosgonzalezchavez09@gmail.com>
2026-02-09 13:52:01 -0600

    Arreglo de rutas y .nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://marcosgonzalez-dev.github.io',
+  site: 'https://marcosgonzalez-dev.github.io/',
 
   server: {
     port: 4321
