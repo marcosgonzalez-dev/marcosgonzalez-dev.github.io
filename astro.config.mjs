7c984b1fee3d49a9d631823c4f9333d52d7f7c0c
marcosgonzalez-dev <marcosgonzalezchavez09@gmail.com>
2026-02-09 14:22:07 -0600

    Arreglo de rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://marcosgonzalez-dev.github.io/',
+  site: 'https://marcosgonzalez-dev.github.io',
+
+  // Agrega esto para cambiar el nombre de la carpeta de estilos
+  build: {
+    assets: 'assets'
+  },
 
   server: {
     port: 4321
